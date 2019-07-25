ac321255fc99e0d00d81cd1f2c807635720700b3
Gleb Grishakov <glebson1988@yandex.ru>
2019-07-25 19:47:27 +0300

    homework of lesson19 att2

--- app.rb
+++ app.rb
@@ -4,16 +4,10 @@ class App
 
   def call(env)
     @request = Rack::Request.new(env)
-    if @request.path_info == '/time'
-      formatter = Formatter.new(@request.params)
-      if formatter.incorrect.empty?
-        answer(200, Time.now.strftime(formatter.correct))
-      else
-        answer(400, "Unknown time format #{formatter.incorrect}")
-      end
-    else
-      answer(404, "Page not found")
-    end
+    return answer(404, 'Page not found') unless @request.path_info == '/time'
+
+    formatter = Formatter.new(@request.params)
+    answer(formatter.status, formatter.body)
   end
 
   private
--- formatter.rb
+++ formatter.rb
@@ -7,17 +7,30 @@ class Formatter
                    "minute" => "%Mm ",
                    "second" => "%Ss" }.freeze
 
-  attr_reader :correct, :incorrect
+  attr_accessor :status, :body
 
   def initialize(params)
     @correct = ''
     @incorrect = []
     time_response(params['format'].split(','))
+    response
   end
 
   private
 
+  def response
+    if @incorrect.empty?
+      self.status = 200
+      self.body = Time.now.strftime(@correct)
+    else
+      self.status = 400
+      self.body = "Unknown time format #{@incorrect}"
+    end
+  end
+
   def time_response(params)
-    params.each { |format| TIME_FORMATS[format] ? @correct += TIME_FORMATS[format] : @incorrect << format }
+    params.each do |format|
+      TIME_FORMATS[format] ? @correct += TIME_FORMATS[format] : @incorrect << format
+    end
   end
 end
